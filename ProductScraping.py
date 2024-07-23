d49da8e01372105a0c3ba0ec7d09a7fcca46f27f
localhost-server <99524394+localhost-server@users.noreply.github.com>
2024-07-23 14:56:25 +0530

    Update ProductScraping.py

--- ProductScraping.py
+++ ProductScraping.py
@@ -90,52 +90,127 @@ async def main():
                 continue
 
         MainInfo = OrderedDict()
+
         try:
             name_section = await new_page.query_selector('h1.title.my-0')
             name = await name_section.inner_text()
             MainInfo['Name'] = name
+            type1=True
         except:
+            name_section = await new_page.query_selector('h1.p-m-0')
+            name = await name_section.inner_text()
+            MainInfo['Name'] = name
+            type1=False
+        finally:
             if await new_page.is_visible('h2.subtitle-404'):
                 print("Maybe the car is sold")
                 collection.update_one({"carLink": carLink}, {"$set": {"Info": "Car Sold Before Scraping"}})
                 continue
 
-        image_section = await new_page.query_selector('.d-flex.thumbImgContainer')
-        if image_section:
-            images = await image_section.query_selector_all('img')
-            image_urls = [await image.get_attribute('src') for image in images]
-            image_urls = [url.replace("thb", "ful") for url in image_urls]
+        if type1:
+            image_section = await new_page.query_selector('.d-flex.thumbImgContainer')
+            if image_section:
+                images = await image_section.query_selector_all('img')
+                image_urls = [await image.get_attribute('src') for image in images]
+                image_urls = [url.replace("thb", "ful") for url in image_urls]
 
-            image_names = []
-            for i in image_urls:
-                numeric_part = re.search(r'\d+', link).group()
-                ImageName = f'{name}-{numeric_part}-{image_urls.index(i)}.jpg'
-                image_names.append(ImageName)
-            print("Images Found")
-            subprocess.Popen(["python", "downloadNupload.py", name, link, json.dumps(image_urls)])
-            
-            MainInfo['Images'] = image_names
-
-        try:
-            vehicle_info = OrderedDict()
-            vinfo = await new_page.wait_for_selector('div.panel-content.d-flex.f-g1.d-flex-column.full-width')
-            vinfo = await vinfo.query_selector('div.f-g2')
-            check = await vinfo.query_selector_all('div.d-flex')
-    
-            while check:
-                try:
-                    label, value = (await check.pop(0).inner_text()).split("\n")
-                    label = label.replace(":", "")
-                    vehicle_info[label] = value
-                    if "******" in value and "VIN" in label:
-                        logged_out=True
-                        # break
-                except:
-                    break
+                image_names = []
+                for i in image_urls:
+                    numeric_part = re.search(r'\d+', link).group()
+                    ImageName = f'{name}-{numeric_part}-{image_urls.index(i)}.jpg'
+                    image_names.append(ImageName)
+                print("Images Found")
+                subprocess.Popen(["python", "downloadNupload.py", name, link, json.dumps(image_urls)])
                 
-            MainInfo['Vehicle Info'] = vehicle_info
-        except:
-            pass
+                MainInfo['Images'] = image_names
+
+
+            try:
+                vehicle_info = OrderedDict()
+                vinfo = await new_page.wait_for_selector('div.panel-content.d-flex.f-g1.d-flex-column.full-width')
+                vinfo = await vinfo.query_selector('div.f-g2')
+                check = await vinfo.query_selector_all('div.d-flex')
+
+                while check:
+                    try:
+                        label, value = (await check.pop(0).inner_text()).split("\n")
+                        label = label.replace(":", "")
+                        vehicle_info[label] = value
+                        if "******" in value and "VIN" in label:
+                            logged_out=True
+                            break
+                    except:
+                        break
+                    
+                MainInfo['Vehicle Info'] = vehicle_info
+            except:
+                pass
+
+
+            try:
+                sale_info = OrderedDict()
+                sinfo = await new_page.wait_for_selector("div.panel.clr.overflowHidden")
+                # sinfo = await new_page.query_selector("div.panel.clr.overflowHidden")
+                check = await sinfo.query_selector_all('div.d-flex')
+        
+                while check:
+                    try:
+                        data = await check.pop(0).inner_text()
+                        if "\n\n" in data:
+                            label, value = data.split("\n\n")
+                            label = label.replace(":", "")
+                        elif "\n" in data:
+                            label, value = data.split("\n", 1)
+                            label = label.replace(":", "")
+                    except:
+                        break
+                    sale_info[label] = value
+        
+                MainInfo['Sale Info'] = sale_info
+            except:
+                pass
+
+        elif not type1:
+            await new_page.click('span.p-ml-3.text-black')
+            await asyncio.sleep(1)
+            images_section = await new_page.query_selector_all('div.p-galleria-thumbnail-items-container')
+            image_section = await images_section[1].query_selector('div.p-galleria-thumbnail-items')
+            if image_section:
+                images = await image_section.query_selector_all('img')
+                image_urls = [await image.get_attribute('src') for image in images]
+                image_urls = [url.replace("thb", "ful") for url in image_urls]
+                image_names = []
+
+                for i in image_urls:
+                    numeric_part = re.search(r'\d+', link).group()
+                    ImageName = f'{name}-{numeric_part}-{image_urls.index(i)}.jpg'
+                    image_names.append(ImageName)
+                print("Images Found")
+                subprocess.Popen(["python", "downloadNupload.py", name, link, json.dumps(image_urls)])
+                
+                MainInfo['Images'] = image_names
+                # Getting back
+                await new_page.click('span.p-pl-3.text-dark-gray-3.p-fs-14')
+
+            try:
+                vehicle_info = OrderedDict()
+                vinfo = await new_page.wait_for_selector('div.lot-details-section.vehicle-info')
+                check = await vinfo.query_selector_all('div.lot-details-info')
+
+                while check:
+                    try:
+                        label, value = (await check.pop(0).inner_text()).split("\n")
+                        label = label.replace(":", "")
+                        vehicle_info[label] = value
+                        if "******" in value and "VIN" in label:
+                            logged_out=True
+                            break
+                    except:
+                        break
+                    
+                MainInfo['Vehicle Info'] = vehicle_info
+            except:
+                pass
 
         if logged_out:
             collection.update_one({"carLink": carLink}, {"$set": {"Info": "None"}})
@@ -161,28 +236,6 @@ async def main():
                 continue
 
 
-        try:
-            sale_info = OrderedDict()
-            sinfo = await new_page.wait_for_selector("div.panel.clr.overflowHidden")
-            # sinfo = await new_page.query_selector("div.panel.clr.overflowHidden")
-            check = await sinfo.query_selector_all('div.d-flex')
-    
-            while check:
-                try:
-                    data = await check.pop(0).inner_text()
-                    if "\n\n" in data:
-                        label, value = data.split("\n\n")
-                        label = label.replace(":", "")
-                    elif "\n" in data:
-                        label, value = data.split("\n", 1)
-                        label = label.replace(":", "")
-                except:
-                    break
-                sale_info[label] = value
-    
-            MainInfo['Sale Info'] = sale_info
-        except:
-            pass
         collection.update_one({"carLink": carLink}, {"$set": {"Info": MainInfo}})
         count += 1
         del Document
