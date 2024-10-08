5947488f38a6e538ebd040a6f0103d773860bbc5
localhost-server <99524394+localhost-server@users.noreply.github.com>
2024-10-08 19:29:50 +0530

    Update AuctionLinkScraping.py

--- AuctionLinkScraping.py
+++ AuctionLinkScraping.py
@@ -14,6 +14,21 @@ async def open_browser(page):
     weblink = "https://www.copart.com/login/"
     await page.goto(weblink, wait_until='load')
     await asyncio.sleep(20)
+    # Find the email input field by its ID
+    email_input = await page.query_selector('#username')
+    email = "matti19913@gmail.com"
+    await email_input.fill(email)
+    password_input = await page.query_selector('#password')
+    # Enter the desired content
+    password = "Copart2023!"
+    await password_input.fill(password)
+    # Clicking on the remember me checkbox
+    await page.click('text=Remember?')
+    # Clicking on the login button
+    await page.click('text=Sign Into Your Account')
+    # Waiting for the page to load
+    await asyncio.sleep(5)
+    return page
 
 async def navigate_to_auctions(page):
     await page.goto("https://www.copart.com/todaysAuction")
@@ -31,9 +46,15 @@ async def get_links(auction, collection):
         print(e)
 
 async def fetch_live_auctions(playwright, collection):
-    while datetime.now(cdt).strftime("%H:%M") <= "23:30":
+
+    while datetime.now(cdt).strftime("%H:%M") < "23:30":
         try:
             print(f"Time: {datetime.now(cdt).strftime('%H:%M')}, Launching browser...")
+
+            # Delete completed auctions before each round of checks
+            collection.delete_many({"Info": "done"})
+            print("Removed auctions marked as 'done'.")
+
             browser = await playwright.firefox.launch(headless=True)
             context = await browser.new_context()
             page = await context.new_page()
@@ -69,7 +90,7 @@ async def fetch_live_auctions(playwright, collection):
             await asyncio.sleep(30)  # Sleep briefly before retrying
 
     print("Time passed 23:30, closing the script.")
-    # Delete entries with "None" info
+    # Delete entries with "None" info before ending the script
     collection.delete_many({"Info": "None"})
 
 async def main():
@@ -78,7 +99,9 @@ async def main():
         client = pymongo.MongoClient(os.getenv("MONGO_URI"))
         db = client['Copart']
         collection = db['AuctionLinks']
-
+        
+        # Clearing all data from collection
+        collection.delete_many({})
         await fetch_live_auctions(playwright, collection)
 
 if __name__ == "__main__":
